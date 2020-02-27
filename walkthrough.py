bb8f763920ef1559f33981b7e677d07b7bbab12e
peter <psvehla@hotmail.com>
2020-02-27 16:55:12 +1100

    Went through the tute.

--- walkthrough.py
+++ walkthrough.py
@@ -1,7 +1,17 @@
 # Put your imports here
+import os
+from web3 import Web3, HTTPProvider
+from interface import ContractInterface
 
 # Initialize your web3 object
+w3 = Web3(HTTPProvider('http://127.0.0.1:8545'))
 
 # Create a path object to your Solidity source files
+contract_dir = os.path.abspath('./contracts/')
 
 # Initialize your interface
+greeter_interface = ContractInterface(w3, 'Greeter', contract_dir)
+greeter_interface.compile_source_files()
+greeter_interface.deploy_contract()
+instance = greeter_interface.get_instance()
+instance.functions.greet().call()
